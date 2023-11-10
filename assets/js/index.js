f3e14b6d2ad98b0500716eae3bb94bde2c654cec
Deividhsn <deividhsn@gmail.com>
2023-11-10 14:49:46 -0300

    Atualização 001

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,19 @@
-var swiper = new Swiper(".mySwiper", {
+// var swiper = new Swiper(".mySwiper", {
 
-    slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+//     slidesPerView: 1,
+//       spaceBetween: 30,
+//       loop: true,
+//       autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
 
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });

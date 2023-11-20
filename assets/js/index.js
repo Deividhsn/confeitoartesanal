ccca6e78b0564d08d7df81581cefa1cc5d1dbade
Deividhsn <deividhsn@gmail.com>
2023-11-20 15:47:30 -0300

    Atualização  all products

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,9 @@
-// var swiper = new Swiper(".mySwiper", {
 
-//     slidesPerView: 1,
-//       spaceBetween: 30,
-//       loop: true,
-//       autoplay: {
-//         delay: 2500,
-//         disableOnInteraction: false,
-//     },
-
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//   });
 $( function() {
-    $( "#accordion" ).accordion();
+    $( "#accordion" ).accordion({
+      collapsible: true,
+      active: false
+    });
+    
   } );
+  

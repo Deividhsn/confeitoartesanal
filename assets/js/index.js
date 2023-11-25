69df8a5dd8b13dcbd344aa74d499384ad95eb64a
Deividhsn <deividhsn@gmail.com>
2023-11-25 14:14:50 -0300

    Atualizações complementares

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+let header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+    header.classList.toggle("alterHeader", window.scrollY > 0);
+});
+
+let body = document.querySelector("body");
+window.addEventListener("scroll", function () {
+    body.classList.toggle("alterBody", window.scrollY > 0);
+});
+
+
+ 
+
 
 $( function() {
     $( "#accordion" ).accordion({
